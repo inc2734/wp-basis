1dbc517b4728ab2564a088a4f5befc530712c904
Takashi Kitajima <inc@2inc.org>
2017-12-11 21:52:48 +0900

    Change iframe to basic non-responsive

--- src/app/setup/embed.php
+++ src/app/setup/embed.php
@@ -14,9 +14,9 @@
  * @param int $post_id
  */
 add_action( 'embed_oembed_html', function( $cache, $url, $attr, $post_id ) {
-	if ( wp_basis_is_16to9_oembed( $url ) ) {
+	if ( wp_basis_is_16to9_oembed_domains( $url ) ) {
 		$cache = '<div class="c-responsive-container-16-9">' . $cache . '</div>';
-	} elseif ( wp_basis_is_4to3_oembed( $url ) ) {
+	} elseif ( wp_basis_is_4to3_oembed_domains( $url ) ) {
 		$cache = '<div class="c-responsive-container-4-3">' . $cache . '</div>';
 	}
 	return $cache;
@@ -30,12 +30,27 @@ add_action( 'embed_oembed_html', function( $cache, $url, $attr, $post_id ) {
  * @return string
  */
 add_filter( 'the_content', function( $content ) {
-	if ( apply_filters( 'inc2734_wp_basis_use_responsive_iframe', true ) ) {
-		$content = preg_replace(
-			'/(<iframe [^>]*?>[^<]*?<\/iframe>)/i',
-			'<div class="c-responsive-container-16-9">$1</div>',
-			$content
+	if ( ! apply_filters( 'inc2734_wp_basis_use_responsive_iframe', true ) ) {
+		preg_match(
+			'/<iframe[^>]*?src=["\']?([^"\'> ]*)["\']?[^<]*?<\/iframe>/i',
+			$content,
+			$reg
 		);
+
+		if ( ! empty( $reg[1] ) ) {
+			$patterns = array_merge(
+				wp_basis_get_16to9_oembed_domains(),
+				wp_basis_get_4to3_oembed_domains()
+			);
+
+			if ( wp_basis_is_oembed_domains( $reg[1] ) ) {
+				$content = preg_replace(
+					'/(<iframe [^>]*?>[^<]*?<\/iframe>)/i',
+					'<div class="c-responsive-container-16-9">$1</div>',
+					$content
+				);
+			}
+		}
 	}
 
 	$content = preg_replace(
@@ -48,15 +63,39 @@ add_filter( 'the_content', function( $content ) {
 } );
 
 /**
- * Return true when 16:9 oEmbed URL
+ * Return true when oEmbed URL
  *
- * @param string  $url
+ * @param string $src
  * @return boolean
  */
-function wp_basis_is_16to9_oembed( $url ) {
-	$patterns = [
-		'https?:\/\/www\.youtube\.com',
+function wp_basis_is_oembed_domains( $src ) {
+	foreach ( $patterns as $pattern ) {
+		if ( preg_match( '/^' . $pattern . '/', $src ) ) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+ * Return 16:9 oembed domains
+ *
+ * @return array
+ */
+function wp_basis_get_16to9_oembed_domains() {
+	return [
+		'(https?:)?\/\/www\.youtube\.com',
 	];
+}
+
+/**
+ * Return true when 16:9 oEmbed URL
+ *
+ * @param string $url
+ * @return boolean
+ */
+function wp_basis_is_16to9_oembed_domains( $url ) {
+	$patterns = wp_basis_get_16to9_oembed_domains();
 	foreach ( $patterns as $pattern ) {
 		if ( preg_match( '/^' . $pattern . '/', $url ) ) {
 			return true;
@@ -66,16 +105,25 @@ function wp_basis_is_16to9_oembed( $url ) {
 }
 
 /**
+ * Return 4:3 oembed domains
+ *
+ * @return array
+ */
+function wp_basis_get_4to3_oembed_domains() {
+	return [
+		'(https?:)?\/\/www\.slideshare\.net',
+		'(https?:)?\/\/speakerdeck\.com',
+	];
+}
+
+/**
  * Return true when 4:3 oEmbed URL
  *
- * @param string  $url
+ * @param string $url
  * @return boolean
  */
-function wp_basis_is_4to3_oembed( $url ) {
-	$patterns = [
-		'https?:\/\/www\.slideshare\.net',
-		'https?:\/\/speakerdeck\.com',
-	];
+function wp_basis_is_4to3_oembed_domains( $url ) {
+	$patterns = wp_basis_get_4to3_oembed_domains();
 	$patterns = apply_filters( 'inc2734_wp_basis_4to3_oembed_domains', $patterns );
 	foreach ( $patterns as $pattern ) {
 		if ( preg_match( '/^' . $pattern . '/', $url ) ) {
