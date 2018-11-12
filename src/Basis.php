bbcc4d9298b9cb3ada7c3caa9072951cd784dda6
Takashi Kitajima <inc@2inc.org>
2018-11-12 11:14:31 +0900

    Update sass-basis@9.2.2 and refactoring

--- src/Basis.php
+++ src/Basis.php
@@ -12,10 +12,10 @@ class Basis {
 	public function __construct() {
 		load_textdomain( 'inc2734-wp-basis', __DIR__ . '/languages/' . get_locale() . '.mo' );
 
-		$includes = array(
-			'/App/setup',
-			'/App/function',
-		);
+		$includes = [
+			'/setup',
+			'/function',
+		];
 		foreach ( $includes as $include ) {
 			foreach ( glob( __DIR__ . $include . '/*.php' ) as $file ) {
 				require_once( $file );
