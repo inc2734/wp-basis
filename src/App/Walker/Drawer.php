350c0a7fbbb6f9e0b55dccdd85487e26087620e7
inc2734 <inc@2inc.org>
2021-08-27 14:48:12 +0900

    some fixes

--- src/App/Walker/Drawer.php
+++ src/App/Walker/Drawer.php
@@ -57,12 +57,12 @@ class Drawer extends \Walker_Nav_Menu {
 
 		if ( $depth > 0 ) {
 			$classes[] = 'c-drawer__subitem';
-			if ( $args->depth && $args->walker->has_children ) {
+			if ( ( 0 === $args->depth || 1 < $args->depth ) && $args->walker->has_children ) {
 				$classes[] = 'c-drawer__subitem--has-submenu';
 			}
 		} else {
 			$classes[] = 'c-drawer__item';
-			if ( $args->depth && $args->walker->has_children ) {
+			if ( ( 0 === $args->depth || 1 < $args->depth ) && $args->walker->has_children ) {
 				$classes[] = 'c-drawer__item--has-submenu';
 			}
 		}
