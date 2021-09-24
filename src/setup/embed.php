f1b35f41285fcde4c95abd31d10ab45b7032c7e9
inc2734 <inc@2inc.org>
2021-09-24 15:35:59 +0900

    https://github.com/inc2734/snow-monkey/issues/1159

--- src/setup/embed.php
+++ src/setup/embed.php
@@ -26,16 +26,25 @@ add_filter(
 		$iframe        = $reg[2];
 		$iframe_after  = $reg[3];
 
-		if ( preg_match( '| width=["\']?(\d+)["\']?|', $iframe, $width ) && preg_match( '| height=["\']?(\d+)["\']?|', $iframe, $height ) ) {
-			$width        = $width[1];
-			$height       = $height[1];
-			$aspect_ratio = $height / $width;
-			if ( 0.55 < $aspect_ratio && 0.57 > $aspect_ratio ) {
-				$cache = $iframe_before . '<div class="c-responsive-container-16-9">' . $iframe . '</div>' . $iframe_after;
-			} else {
-				$cache = $iframe_before . '<div class="c-responsive-container-4-3">' . $iframe . '</div>' . $iframe_after;
+		$has_width_and_height = preg_match( '| width=["\']?(\d+)["\']?|', $iframe, $width )
+													&& preg_match( '| height=["\']?(\d+)["\']?|', $iframe, $height );
+
+		if ( $has_width_and_height ) {
+			$width  = $width[1];
+			$height = $height[1];
+			$is_px  = false === strpos( $width, '%' ) && false === strpos( $height, '%' );
+
+			if ( $is_px ) {
+				$aspect_ratio = $height / $width;
+				if ( 0.55 < $aspect_ratio && 0.57 > $aspect_ratio ) {
+					$cache = $iframe_before . '<div class="c-responsive-container-16-9">' . $iframe . '</div>' . $iframe_after;
+				} elseif ( 0.74 < $aspect_ratio && 0.76 > $aspect_ratio ) {
+					$cache = $iframe_before . '<div class="c-responsive-container-4-3">' . $iframe . '</div>' . $iframe_after;
+				}
 			}
-		} elseif ( wp_basis_is_16to9_oembed_domains( $url ) ) {
+		}
+
+		if ( wp_basis_is_16to9_oembed_domains( $url ) ) {
 			$cache = $iframe_before . '<div class="c-responsive-container-16-9">' . $iframe . '</div>' . $iframe_after;
 		} elseif ( wp_basis_is_4to3_oembed_domains( $url ) ) {
 			$cache = $iframe_before . '<div class="c-responsive-container-4-3">' . $iframe . '</div>' . $iframe_after;
@@ -73,6 +82,60 @@ add_filter(
 		// @codingStandardsIgnoreEnd
 
 		return $cache;
+	}
+);
+
+/**
+ * Make sure oEmbed REST Requests apply the WP Embed security mechanism for WordPress embeds.
+ *
+ * @see  https://core.trac.wordpress.org/ticket/32522
+ * @see  https://github.com/WordPress/gutenberg/blob/master/lib/rest-api.php
+ * @copyright  https://github.com/WordPress/gutenberg
+ *
+ * @param  WP_HTTP_Response|WP_Error $response The REST Request response.
+ * @param  WP_REST_Server            $handler  ResponseHandler instance (usually WP_REST_Server).
+ * @param  WP_REST_Request           $request  Request used to generate the response.
+ * @return WP_HTTP_Response|object|WP_Error    The REST Request response.
+ */
+add_action(
+	'init',
+	function() {
+		if ( wp_basis_is_block_embed_rendering_request() ) {
+			add_filter(
+				'rest_request_after_callbacks',
+				function( $response, $handler, $request ) {
+					if ( '/oembed/1.0/proxy' !== $request->get_route() ) {
+						return $response;
+					}
+
+					if ( empty( $_GET['url'] ) ) {
+						return $response;
+					}
+
+					if ( isset( $response->html ) ) {
+						$has_percent_width  = preg_match( '|^<iframe[^>]+ width="\d+%"|ms', $response->html );
+						$has_percent_height = preg_match( '|^<iframe[^>]+ height="\d+%"|ms', $response->html );
+						if ( ! $has_percent_width && ! $has_percent_height || $has_percent_width && $has_percent_height ) {
+							return $response;
+						}
+					}
+
+					if ( $has_percent_width ) {
+						// Set dummy width
+						$response->html = str_replace( 'width="100%"', 'width="100000"', $response->html );
+					}
+
+					if ( $has_percent_height ) {
+						// Set dummy height
+						$response->html = str_replace( 'height="100%"', 'height="100000"', $response->html );
+					}
+
+					return $response;
+				},
+				10,
+				3
+			);
+		}
 	}
 );
 
@@ -162,4 +225,18 @@ function wp_basis_is_4to3_oembed_domains( $url ) {
 		}
 	}
 	return false;
+}
+
+/**
+ * Return true when block embed rendering request.
+ *
+ * @return boolean
+ */
+function wp_basis_is_block_embed_rendering_request() {
+	if ( ! isset( $_SERVER['REQUEST_URI'] ) ) {
+		return false;
+	}
+
+	return false !== strpos( $_SERVER['REQUEST_URI'], '/wp-json/oembed/1.0/proxy?url=' )
+			|| false !== strpos( $_SERVER['REQUEST_URI'], urlencode( '/oembed/1.0/proxy' ) );
 }
