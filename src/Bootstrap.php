eda64656869c55c2c801dea2b0322f37894c4209
inc2734 <inc@2inc.org>
2024-11-05 15:37:14 +0900

    inc2734/snow-monkey#1449 load_theme_textdomain() を init から後に変更

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -13,8 +13,14 @@ class Bootstrap {
 	 * Constructor.
 	 */
 	public function __construct() {
-		load_textdomain( 'inc2734-wp-basis', __DIR__ . '/languages/' . get_locale() . '.mo' );
+		add_action(
+			'init',
+			function() {
+				load_textdomain( 'inc2734-wp-basis', __DIR__ . '/languages/' . get_locale() . '.mo' );
+			}
+		);
 
+		include_once __DIR__ . '/setup/i18n.php';
 		include_once __DIR__ . '/setup/nav-menus.php';
 		include_once __DIR__ . '/setup/body-class.php';
 		include_once __DIR__ . '/setup/comment-form.php';
